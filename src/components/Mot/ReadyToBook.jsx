9e199c0ac5aa5c60df6822b1750bf1b2304258b2
STharshan <sivatharshan81@gmail.com>
2025-09-28 18:03:11 +0530

    add

--- src/components/Mot/ReadyToBook.jsx
+++ src/components/Mot/ReadyToBook.jsx
@@ -13,10 +13,12 @@ export default function ReadyToBook() {
                         <FiCalendar className="mr-2 h-5 w-5" />
                         Book Now
                     </button>
-                    <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium disabled:pointer-events-none disabled:opacity-50 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border dark:bg-input/300 shadow-lg dark:border-input hover:bg-red-700 hover:shadow-lg  active:bg-red-800 transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 dark:hover:bg-input/50 h-10 rounded-md px-6 border-primary-foreground text-primary-foreground hover:bg-primary-foreground hover:text-primary bg-transparent">
-                        <FiPhone className="mr-2 h-5 w-5" />
-                        Call Us Today
-                    </button>
+                    <a href="tel: 02476 638999">
+                        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium disabled:pointer-events-none disabled:opacity-50 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border dark:bg-input/300 shadow-lg dark:border-input hover:bg-red-700 hover:shadow-lg  active:bg-red-800 transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 dark:hover:bg-input/50 h-10 rounded-md px-6 border-primary-foreground text-primary-foreground hover:bg-primary-foreground hover:text-primary bg-transparent">
+                            <FiPhone className="mr-2 h-5 w-5" />
+                            Call Us Today
+                        </button>
+                    </a>
                 </div>
             </div>
         </section>
