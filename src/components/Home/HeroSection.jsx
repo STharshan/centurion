1e3e45564463ead81515cc46905b6afd619a496f
STharshan <sivatharshan81@gmail.com>
2026-01-07 22:38:14 +0530

    a

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -13,12 +13,12 @@ const HeroSection = () => {
         playsInline
         className="absolute inset-0 w-full h-full object-cover z-0"
       >
-        <source src="/garage.mp4" type="video/mp4" />
+        <source src="/bg.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-60 z-10" />
+      <div className="absolute inset-0 bg-black/50 bg-opacity-60 z-10" />
 
       {/* Content */}
       <div className="relative z-20 h-full flex flex-col justify-center px-6 md:px-25 mt-20">
